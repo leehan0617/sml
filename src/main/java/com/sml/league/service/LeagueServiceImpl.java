144362a12404f049ca9bfb7acca63ba873123b5d
leeheejae91 <91moderato@gmail.com>
2015-07-13 20:50:55 +0900

    15. 07. 13. 리그 스케쥴링을 위한 요일에 해당하는 날짜 추출 완료

--- src/main/java/com/sml/league/service/LeagueServiceImpl.java
+++ src/main/java/com/sml/league/service/LeagueServiceImpl.java
@@ -1,5 +1,8 @@
 package com.sml.league.service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,8 +66,70 @@ public class LeagueServiceImpl implements LeagueService{
 		
 	}
 	
+	/**
+	 * @name : leagueSchedule
+	 * @date : 2015. 7. 13.
+	 * @author : 이희재
+	 * @description : 리그 신청 후 정원이 꽉차면 요일에 따른 날짜 구하기
+	 */
 	public void leagueSchedule(int leagueCode){
 		LeagueDto league=dao.getLeagueInfo(leagueCode);
+		int teamCount=league.getLeagueTeamNumber();
 		
+		// 총 경기 수
+		int gameCount=(teamCount*(teamCount-1))/2;
+		
+		ArrayList<Date> dateList=new ArrayList<Date>();
+		
+		// 주말 날짜 구하기
+		Date date=new Date();
+		Date dateSun=new Date();
+		
+		Calendar cal=Calendar.getInstance();
+		cal.setTime(new java.util.Date(System.currentTimeMillis()));
+		
+		int day=date.getDay();
+		
+		if(day<6){
+			// 이번주 토요일
+			cal.add(cal.DAY_OF_WEEK, 6-day);
+			date=cal.getTime();
+			
+			// 이번주 일요일
+			cal.add(cal.DAY_OF_WEEK, 1);
+			dateSun=cal.getTime();
+		}
+		
+		// 토요일 날짜
+		cal.setTime(date);
+		dateList.add(date);
+		
+		for(int i=0;i<10;i++){
+			cal.add(Calendar.DATE, 7);
+			Date tempDate=cal.getTime();
+			dateList.add(tempDate);
+			cal.setTime(tempDate);
+		}
+		
+		for(int i=0;i<dateList.size();i++){
+			System.out.println(dateList.get(i));
+		}
+		
+		System.out.println("일요일------------------");
+		// 일요일 날짜
+		dateList=new ArrayList<Date>();
+		cal.setTime(dateSun);
+		dateList.add(dateSun);
+		
+		for(int i=0;i<10;i++){
+			cal.add(Calendar.DATE, 7);
+			Date tempDate=cal.getTime();
+			dateList.add(tempDate);
+			cal.setTime(tempDate);
+		}
+		
+		for(int i=0;i<dateList.size();i++){
+			System.out.println(dateList.get(i));
+		}
 	}
 }
