583c6b46b938571dddda4dfae7d21c9a67372bdf
이한빈 <leehan0617@naver.com>
2015-07-15 12:51:23 +0900

    ccccc

--- src/main/java/com/sml/team/controller/TeamController.java
+++ src/main/java/com/sml/team/controller/TeamController.java
@@ -107,7 +107,7 @@ public class TeamController {
 		
 		mav.addObject("request",request);
 		service.goTeamPage(mav);
-		teamBoardService.viewTeamBoard(mav);
+		//teamBoardService.viewTeamBoard(mav);
 		scheduleService.readteamSchedule(mav);
 		service.getReplyList(mav);
 		
