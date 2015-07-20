1eeb47948a45971002af8675113484d6703f5cb3
leeheejae91 <91moderato@gmail.com>
2015-07-20 09:36:24 +0900

    5555

--- src/main/java/com/sml/admin/controller/AdminController.java
+++ src/main/java/com/sml/admin/controller/AdminController.java
@@ -317,22 +317,22 @@ public class AdminController {
 			return mav;
 		}
 		
-		/**
-		 * @name : insertGameResult
-		 * @date : 2015. 7. 17.
-		 * @author : 이희재
-		 * @description : 리그의 정보를 가져오고 그에 대한 결과를 입력하는 메소드
-		 */
-		@RequestMapping(value="/admin/manageLeagueRecord.do",method=RequestMethod.GET)
-		public ModelAndView insertGameResult(HttpServletRequest request){
-			logger.info("insertGameResult-----------");
-			ModelAndView mav=new ModelAndView();
-			mav.addObject("request",request);
-
-			adminService.insertGameResult(mav);
-			return mav;
-		}
-		
+//		/**
+//		 * @name : insertGameResult
+//		 * @date : 2015. 7. 17.
+//		 * @author : 이희재
+//		 * @description : 리그의 정보를 가져오고 그에 대한 결과를 입력하는 메소드
+//		 */
+//		@RequestMapping(value="/admin/manageLeagueRecord.do",method=RequestMethod.GET)
+//		public ModelAndView insertGameResult(HttpServletRequest request){
+//			logger.info("insertGameResult-----------");
+//			ModelAndView mav=new ModelAndView();
+//			mav.addObject("request",request);
+//
+//			adminService.insertGameResult(mav);
+//			return mav;
+//		}
+//		
 		
 		
 }
