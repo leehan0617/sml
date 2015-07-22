0718c3d9e89b53deaa95625e87477ea939c12205
jayjung1369 <jayjung1369@naver.com>
2015-07-22 12:16:46 +0900

    정성남-레그볼-팀인포수정

---
+++ src/main/java/com/sml/legBall/dao/LegBallDao.java
@@ -0,0 +1,39 @@
+package com.sml.legBall.dao;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import com.sml.common.dto.CommonBoardDto;
+import com.sml.league.dto.LeagueDto;
+import com.sml.member.dto.MemberDto;
+import com.sml.record.dto.RecordDto;
+
+public interface LegBallDao {
+	public int getBoardCount(int sportCode);
+
+	public List<CommonBoardDto> getlegBallBoardList(int sportCode, int startRow,
+			int endRow);
+
+	public CommonBoardDto getBoardContent(int boardNumber);
+
+	public List<MemberDto> showAgeChart(String sportType);
+
+	public List<Date> showDayChart(String sportType);
+	
+	public List<HashMap<String,Object>> getAllTeamList(String sportType,int startRow,int endRow);
+
+	public List<LeagueDto> showLeagueInfo(String sportType);
+
+	public List<LeagueDto> getLeagueJoin();
+
+	public int joinLeague(HashMap<String, Object> hMap);
+
+	public int checkLeague(HashMap<String, Object> hMap);
+	
+	public int teamCount(String sportType);
+
+	public List<RecordDto> showRecentMatch(String gameType);
+
+	public String findMatchTeams(int parseInt);
+}
---
+++ src/main/java/com/sml/legBall/service/LegBallService.java
@@ -0,0 +1,34 @@
+package com.sml.legBall.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public interface LegBallService {
+	public ModelAndView viewlegBallBoard(HttpServletRequest request);
+
+	public ModelAndView readlegBallBoard(HttpServletRequest request);
+
+	public ModelAndView showAgeChart(HttpServletRequest request);
+
+	public ModelAndView showCityChart(HttpServletRequest request);
+
+	public ModelAndView showDayChart(HttpServletRequest request);
+	
+	public void legBallRule(ModelAndView mav);
+	
+	public void legBallTeamList(ModelAndView mav);
+
+	public ModelAndView showLeagueInfo(HttpServletRequest request);
+
+	public ModelAndView leagueTable(HttpServletRequest request);
+
+	public ModelAndView joinLeague(HttpServletRequest request);
+	
+	public void legBallCommonBoard(ModelAndView mav);
+
+	public ModelAndView showRecentMatch(HttpServletRequest request);
+
+	public ModelAndView findMatchTeams(HttpServletRequest request);
+
+}
